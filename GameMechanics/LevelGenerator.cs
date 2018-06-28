149bf49d2987a3e37a7a24cd693bb4d9883acdcf
Shadman Kudchikar <shadmanssk@yahoo.in>
2018-06-28 15:45:01 +0530

    Add project files.

---
+++ GameMechanics/LevelGenerator.cs
@@ -0,0 +1,18 @@
+﻿using SudokuSharp;
+
+namespace GameMechanics
+{
+    public class LevelGenerator
+    {
+        public static Board GenerateBoard(int seed, int level)
+        {
+            var cuts = (int)((((level * 0.0017) + 0.37) * 81));            
+
+            if (level < 50)
+                return Factory.Puzzle(seed + level, 0, 0, cuts);
+            else if (level < 150)
+                return Factory.Puzzle(seed + level, 0, (cuts / 3), (cuts / 3) * 2);
+            return Factory.Puzzle(seed + level, 0, cuts, 0);
+        }
+    }
+}
