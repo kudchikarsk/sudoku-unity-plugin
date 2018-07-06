b0d6f2fa2cd88a94f15e355d926011269ec6c5b2
Shadman Kudchikar <shadmanssk@yahoo.in>
2018-07-06 23:28:10 +0530

    add history and pencid grid

---
+++ SudokuSharp/History/PencilMarks.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.Serialization;
+
+namespace SudokuSharp.History
+{
+    internal abstract class PencilActionBase
+    {
+        internal PencilActionBase(PencilGrid Target, Location Where, int Value)
+        {
+            _target = Target;
+            _where = Where;
+            _value = Value;
+        }
+        
+        protected PencilGrid _target;
+        protected Location _where;
+        protected int _value;
+        protected bool _prior;
+    }
+    
+    internal class PencilToggle : PencilActionBase, IHistoryAction
+    {
+        public PencilToggle(PencilGrid Target, Location Where, int Value) : base(Target, Where, Value) { }
+        public void Apply() { _target.Toggle(_where, _value); }
+        public void Undo() { Apply(); }
+    }
+
+    internal class PencilSet : PencilActionBase, IHistoryAction
+    {
+        public PencilSet(PencilGrid Target, Location Where, int Value) : base(Target, Where, Value) { }
+        public void Apply()
+        {
+            _prior = _target.Get(_where, _value);
+            _target.Set(_where, _value);
+        }
+        public void Undo() {
+            if (!_prior)
+                _target.Clear(_where, _value);
+        }
+    }
+
+    internal class PencilClear : PencilActionBase, IHistoryAction
+    {
+        public PencilClear(PencilGrid Target, Location Where, int Value) : base(Target, Where, Value) { }
+        public void Apply()
+        {
+            _prior = _target.Get(_where, _value);
+            _target.Clear(_where, _value);
+        }
+        public void Undo()
+        {
+            if (_prior)
+                _target.Set(_where, _value);
+        }
+    }
+}
+
